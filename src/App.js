e49a076beea4abd5f69fa2735d01565ff1158202
louashka <louashka@gmail.com>
2023-08-11 00:11:18 +0800

    Update the root link

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Layout from "./pages/Layout";
 import Home from './pages/Home';
-import Fabrication from './pages/Fabrication';
+import Fabrication from "./pages/Fabrication";
 import FabTutorial from "./pages/FabTutorial";
 import Downloads from './pages/Downloads';
 import Projects from './pages/Projects';
@@ -10,7 +10,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route path="/dsm-web-app/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="fabrication" element={<Fabrication />} />
           <Route path="fabrication/:id" element={<FabTutorial />} />
